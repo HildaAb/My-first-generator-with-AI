cd20a3116bbded265198df3197fda61064ac026b
HildaAb <ilona.aase@gmail.com>
2024-10-12 12:56:54 +0200

    added basic js and updated style in css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function generateRecepy(event) {
+  event.preventDefault();
 
+  new Typewriter("#recepy", {
+    strings: ["Budapest"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let userInputElement = document.querySelector("#user-input");
+userInputElement.addEventListener("submit", generateRecepy);
